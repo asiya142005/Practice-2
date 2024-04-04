baa0efe8c12346f7aa2fbdd0ab253152afbcf638
Asiya <asiyacharolia@gmail.com>
2024-04-05 01:28:56 +0530

    add to addition in add.cpp

--- add2.cpp
+++ add2.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-int add(int a,int b)
+int addition(int a,int b)
 {
 	cout<<"\n"<<a+b;
 }
@@ -11,5 +11,5 @@ int main()
 	cin>>x;
 	cout<<"enter values for y:";
 	cin>>y;
-	add(x,y);
+	addition(x,y);
 }
