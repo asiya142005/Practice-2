ad136b90ee8d543e69d155fcfbed5b350d090b52
Asiya <asiyacharolia@gmail.com>
2024-04-05 01:24:16 +0530

    added add.cpp

---
+++ add2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int add(int a,int b)
+{
+	cout<<"\n"<<a+b;
+}
+int main()
+{
+	int x,y,result;
+	cout<<"enter values for x:";
+	cin>>x;
+	cout<<"enter values for y:";
+	cin>>y;
+	add(x,y);
+}
